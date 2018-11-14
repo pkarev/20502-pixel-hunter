49d9b56c3bea99999b11b733a4318dcd1ba03958
pavelkarev <pavel.karev@pegast.ru>
2018-11-14 23:02:22 +0300

    Add screen render function

--- js/main.js
+++ js/main.js
@@ -3,6 +3,9 @@
 const templatesNodeList = document.querySelectorAll(`template`);
 const screenIds = [];
 const screens = [];
+const main = document.querySelector(`#main`);
+
+let currentScreenIndex = 0;
 
 const getScreenIds = () => {
   templatesNodeList.forEach((node) => {
@@ -15,6 +18,16 @@ const getScreenTemplates = () => {
     const screen = document.querySelector(`#` + id + ``);
     screens.push(screen);
   });
+};
+
+const renderScreen = (screenIndex) => {
+  const shadow = document.createElement(`div`);
+  const content = screens[screenIndex].content.cloneNode(true);
+  shadow.appendChild(content);
+  const screenContent = shadow.cloneNode(true);
+  main.innerHTML = ``;
+  main.appendChild(screenContent);
+  currentScreenIndex = (screenIndex);
 };
 
 getScreenIds();
