ae1d33197fd917b2e1f964c8e0f22f95ef9d1cf5
pavelkarev <pavel.karev@pegast.ru>
2018-11-14 23:13:00 +0300

    Configure screen switching via keyboard arrows

--- js/main.js
+++ js/main.js
@@ -5,6 +5,11 @@ const screenIds = [];
 const screens = [];
 const main = document.querySelector(`#main`);
 
+const KeyCode = {
+  LEFT_ARROW: 37,
+  RIGHT_ARROW: 39
+};
+
 let currentScreenIndex = 0;
 
 const getScreenIds = () => {
@@ -30,6 +35,44 @@ const renderScreen = (screenIndex) => {
   currentScreenIndex = (screenIndex);
 };
 
+const isLeftArrowKeyup = (evt, action) => {
+  if (evt.keyCode !== KeyCode.LEFT_ARROW) {
+    return;
+  }
+  action();
+};
+
+const isRightArrowKeyup = (evt, action) => {
+  if (evt.keyCode !== KeyCode.RIGHT_ARROW) {
+    return;
+  }
+  action();
+};
+
+const renderPreviousScreen = () => {
+  if (currentScreenIndex === 0) {
+    return;
+  }
+  currentScreenIndex--;
+  renderScreen(currentScreenIndex);
+};
+
+const renderNextScreen = () => {
+  if (currentScreenIndex === screens.length - 1) {
+    return;
+  }
+  currentScreenIndex++;
+  renderScreen(currentScreenIndex);
+};
+
+const onDocumentArrowKeyUp = (evt) => {
+  evt.preventDefault();
+  isLeftArrowKeyup(evt, renderPreviousScreen);
+  isRightArrowKeyup(evt, renderNextScreen);
+};
+
 getScreenIds();
 getScreenTemplates();
 renderScreen(1);
+
+document.addEventListener(`keyup`, onDocumentArrowKeyUp);
