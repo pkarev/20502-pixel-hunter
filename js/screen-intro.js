11f40332c9a9c0ced83f0fe3e8d3517d39448267
pavelkarev <pavel.karev@pegast.ru>
2018-11-17 22:14:51 +0300

    Create intro screen module

---
+++ js/screen-intro.js
@@ -0,0 +1,12 @@
+import createDomElementFromStringTemplate from './create-dom-element';
+
+const introScreenTemplate = `
+<section class="intro">
+  <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+  <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+</section>
+`;
+
+const introScreenElement = createDomElementFromStringTemplate(introScreenTemplate);
+
+export default introScreenElement;
