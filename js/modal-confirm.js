f5b7924b8d2d9410f88619582528b4ca94be5731
pavelkarev <pavel.karev@pegast.ru>
2018-11-18 23:46:24 +0300

    Create modal confirm module

---
+++ js/modal-confirm.js
@@ -0,0 +1,21 @@
+import createDomElementFromStringTemplate from "./create-dom-element";
+
+const modalConfirmTemplate = `
+<section class="modal">
+    <form class="modal__inner">
+      <button class="modal__close" type="button">
+        <span class="visually-hidden">Закрыть</span>
+      </button>
+      <h2 class="modal__title">Подтверждение</h2>
+      <p class="modal__text">Вы уверены что хотите начать игру заново?</p>
+      <div class="modal__button-wrapper">
+        <button class="modal__btn">Ок</button>
+        <button class="modal__btn">Отмена</button>
+      </div>
+    </form>
+  </section>
+`;
+
+const modalConfirmElement = createDomElementFromStringTemplate(modalConfirmTemplate);
+
+export default modalConfirmElement;
