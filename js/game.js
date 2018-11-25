f1db90c4d120dc23078fa481655c3fb0afc96ac1
pavelkarev <pavel.karev@pegast.ru>
2018-11-25 17:57:11 +0300

    Test game lives changer

--- js/game.js
+++ js/game.js
@@ -4,6 +4,8 @@ export const INITIAL_GAME_STATE = Object.freeze({
   time: 30
 });
 
+export const MAX_LIVES = 3;
+
 export const changeLevel = (gameState, level) => {
   if (typeof level !== `number`) {
     throw new Error(`New game level must be a number`);
@@ -14,5 +16,22 @@ export const changeLevel = (gameState, level) => {
   }
 
   const newGameState = Object.assign({}, gameState, {level});
+  return newGameState;
+};
+
+export const changeLives = (gameState, lives) => {
+  if (typeof lives !== `number`) {
+    throw new Error(`New game lives must be a number`);
+  }
+
+  if (lives < 0) {
+    throw new Error(`Lives can't be negative. Game over`);
+  }
+
+  if (lives > MAX_LIVES) {
+    throw new Error(`Lives can't be too many`);
+  }
+
+  const newGameState = Object.assign({}, gameState, {lives});
   return newGameState;
 };
--- js/tests/game.test.js
+++ js/tests/game.test.js
@@ -1,5 +1,5 @@
 import {assert} from 'chai';
-import {changeLevel, INITIAL_GAME_STATE} from '../game';
+import {changeLevel, changeLives, INITIAL_GAME_STATE} from '../game';
 
 describe(`Change level`, () => {
   it(`should update level of the game`, () => {
@@ -18,5 +18,28 @@ describe(`Change level`, () => {
     assert.throws(() => changeLevel(INITIAL_GAME_STATE, []).level, /New game level must be a number/);
     assert.throws(() => changeLevel(INITIAL_GAME_STATE, {}).level, /New game level must be a number/);
     assert.throws(() => changeLevel(INITIAL_GAME_STATE, undefined).level, /New game level must be a number/);
+  });
+});
+
+describe(`Change lives`, () => {
+  it(`should update lives of the game`, () => {
+    assert.equal(changeLives(INITIAL_GAME_STATE, 1).lives, 1);
+    assert.equal(changeLives(INITIAL_GAME_STATE, 2).lives, 2);
+    assert.equal(changeLives(INITIAL_GAME_STATE, 3).lives, 3);
+  });
+
+  it(`should no accept negative values`, () => {
+    assert.throws(() => changeLives(INITIAL_GAME_STATE, -1).lives, /Lives can't be negative. Game over/);
+  });
+
+  it(`should not set lives more than allowed max lives number`, () => {
+    assert.throws(() => changeLives(INITIAL_GAME_STATE, 4).lives, `Lives can't be too many`);
+    assert.throws(() => changeLives(INITIAL_GAME_STATE, 100).lives, `Lives can't be too many`);
+  });
+
+  it(`should not allow set non number value`, () => {
+    assert.throws(() => changeLives(INITIAL_GAME_STATE, []).lives, /New game lives must be a number/);
+    assert.throws(() => changeLives(INITIAL_GAME_STATE, {}).lives, /New game lives must be a number/);
+    assert.throws(() => changeLives(INITIAL_GAME_STATE, undefined).lives, /New game lives must be a number/);
   });
 });
