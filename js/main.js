f59153e36cc42047b0d4362dc1f98541f97de344
pavelkarev <pavel.karev@pegast.ru>
2018-12-08 21:03:45 +0300

    Remove screens switcher
    - will be added in the end of a project

--- js/main.js
+++ js/main.js
@@ -1,7 +1,4 @@
 import renderScreen from "./render-screen";
 import introScreenElement from "./screen-intro";
-import * as screensSwitcher from "./screens-switcher";
 
 renderScreen(introScreenElement);
-screensSwitcher.addScreenSwitchingButtons();
-screensSwitcher.activateKeyboardScreenSwitching();
