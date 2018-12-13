6b8bf80afbbd7f0f98d14034ae0d08028c88045c
pavelkarev <pavel.karev@pegast.ru>
2018-12-13 10:30:27 +0300

    Create static screens views

---
+++ js/intro-view.js
@@ -0,0 +1,24 @@
+import {AbstractView} from "./abstract-view";
+
+export default class IntroView extends AbstractView {
+  get template() {
+    return `
+      <section class="intro">
+        <button class="intro__asterisk asterisk" type="button"><span class="visually-hidden">Продолжить</span>*</button>
+        <p class="intro__motto"><sup>*</sup> Это не фото. Это рисунок маслом нидерландского художника-фотореалиста Tjalf Sparnaay.</p>
+      </section>
+    `;
+  }
+
+  onNextScreenClick() {};
+
+  bind() {
+    const nextScreenButton = this.element.querySelector(`.asterisk`);
+
+    nextScreenButton.addEventListener(`click`, (evt) => {
+      evt.preventDefault();
+
+      this.onNextScreenClick();
+    });
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,4 +1,29 @@
 import renderScreen from "./render-screen";
-import introScreenElement from "./screen-intro";
 
-renderScreen(introScreenElement);
+import IntroView from "./intro-view";
+import GreetingView from "./greeting-view";
+import RulesView from "./rules-view";
+import GameView from "./game-view";
+
+const introView = new IntroView();
+const greetingView = new GreetingView();
+const rulesView = new RulesView();
+const gameView = new GameView();
+
+renderScreen(introView.element);
+
+introView.onNextScreenClick = () => {
+  renderScreen(greetingView.element);
+};
+
+greetingView.onNextScreenClick = () => {
+  renderScreen(rulesView.element);
+};
+
+rulesView.onGoHomeClick = () => {
+  renderScreen(introView.element);
+};
+
+rulesView.onNextScreenClick = () => {
+  console.log(`go next`);
+};
