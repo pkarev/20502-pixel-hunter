7a6dba50801878d7d4774b361e540f7b08852b2a
pavelkarev <pavel.karev@pegast.ru>
2018-12-18 20:10:55 +0300

    Use go-home-element in scores screen

--- js/views/scores-view.js
+++ js/views/scores-view.js
@@ -32,15 +32,6 @@ export default class ScoresView extends AbstractView {
   get template() {
     return `
     <header class="header">
-      <button class="back">
-        <span class="visually-hidden">Вернуться к началу</span>
-        <svg class="icon" width="45" height="45" viewBox="0 0 45 45" fill="#000000">
-          <use xlink:href="img/sprite.svg#arrow-left"></use>
-        </svg>
-        <svg class="icon" width="101" height="44" viewBox="0 0 101 44" fill="#000000">
-          <use xlink:href="img/sprite.svg#logo-small"></use>
-        </svg>
-      </button>
     </header>
     <section class="result">
       <h2 class="result__title">${getStatsTitle(this.game.state.isWin)}</h2>
@@ -80,17 +71,5 @@ export default class ScoresView extends AbstractView {
       </table>
       </section>
     `;
-  }
-
-  onGoHomeClick() {}
-
-  bind() {
-    const goHome = this.element.querySelector(`.back`);
-
-    goHome.addEventListener(`click`, (evt) => {
-      evt.preventDefault();
-
-      this.onGoHomeClick();
-    });
   }
 }
